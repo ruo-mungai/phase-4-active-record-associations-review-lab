8a519f48bff1680acee96e6fa95b8cdfd1fdbbb9
Patrick <ruomungai@gmail.com>
2022-09-28 17:03:22 +0300

    done

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,4 @@
 class Ride < ApplicationRecord
+    belongs_to :tax
+    belongs_to :passenger
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,4 @@
 class Taxi < ApplicationRecord
+    has_many :rides
+    has_many :passagers,through: :rides
 end
